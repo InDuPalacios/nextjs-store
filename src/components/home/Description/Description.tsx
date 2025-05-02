a98db2bf32b2c3079ee93207f07d0c851253d5e7
Indu <indira.duranp@gmail.com>
2025-05-02 21:46:52 +0200

    apply object-fit cover and styling to image inside imageContainer

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -10,14 +10,13 @@ import style from './Description.module.scss';
 export const Description = () => {
     return(
         <section className={style.Description}>
-            <Image 
-                src="/images/mishtycus_9.jpg" 
-                alt="products marketplace" 
-                width={500} 
-                height={400}
-                priority={false}
-                quality={30}
-            />
+            <div className={style.Description__imageContainer}>
+                <Image 
+                    src="/images/mishtycus_9.jpg" 
+                    alt="products marketplace" 
+                    fill
+                />
+            </div>
             <div className={style.Description__text}>
                 <h2> Let Mistycus guide you, one meow at a time.</h2>
                 <p>At Mistycus, the keepers of wisdom don’t wear 
@@ -25,7 +24,8 @@ export const Description = () => {
                     cats have gathered the universe’s secrets
                     and the art of the perfect nap — into enchanted 
                     boxes filled with quirky advice, soft spells, and 
-                    oddly useful magic.</p>
+                    oddly useful magic.
+                </p>
             </div>
         </section>
     )
