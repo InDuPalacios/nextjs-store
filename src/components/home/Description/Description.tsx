29b782d27dc184c6a85feb2533aaef7187fb3c6b
Indu <indira.duranp@gmail.com>
2025-05-02 11:34:06 +0200

    Implement Next.js Image component for optimized image rendering

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -2,6 +2,7 @@
 // Description: TODO
 
 // Importing External Modules
+import Image from 'next/image';
 
 // Importing Internal Modules
 import style from './Description.module.scss';
@@ -9,7 +10,14 @@ import style from './Description.module.scss';
 export const Description = () => {
     return(
         <section className={style.Description}>
-            <img src="/images/mishtycus_9.jpg" alt="products marketplace" />
+            <Image 
+                src="/images/mishtycus_9.jpg" 
+                alt="products marketplace" 
+                width={500} 
+                height={400}
+                priority={false}
+                quality={30}
+            />
             <div className={style.Description__text}>
                 <h2> Let Mistycus guide you, one meow at a time.</h2>
                 <p>At Mistycus, the keepers of wisdom don’t wear 
